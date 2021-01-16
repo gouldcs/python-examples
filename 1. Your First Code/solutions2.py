b563f3b9dd9b580121dc74951d6a9da365c43db0
Cameron Gould <56109943+gouldcs@users.noreply.github.com>
2021-01-15 18:00:50 -0800

    Create solutions2.py

---
+++ 1. Your First Code/solutions2.py
@@ -0,0 +1,54 @@
+"""Section 2: Arithmetic
+"""
+SECTION = "\n***********************************\n"
+
+
+# 2. Arithmetic
+print("2. Arithmetic", SECTION + "Example 2.1:")
+# Example 1:
+print(2 + 2)
+
+
+print(SECTION + "Example 2.2:")
+# Example 2:
+print("The United States has" ,  25 + 25 , "states!")
+
+
+print(SECTION + "Example 2.3:")
+# Example 3:
+print(2 + 2)
+print(10 / 2)
+print(5 * 5)
+print(10 - 5)
+
+
+print(SECTION + "Example 2.4:")
+# Example 4:
+print("2 + 2 =", 2 + 2)
+print("10 / 2 =", 10 / 2)
+print("5 * 5 =", 5 * 5)
+print("10 - 5 =", 10 - 5)
+
+
+print(SECTION)
+print("Further Exploration Solutions")
+
+
+print(SECTION + "Problem 1:")
+# Problem 1:
+print("Laura is", 6, "and Jane is", 12, ", the sum of their age is", 6 + 12)
+
+
+print(SECTION + "Problem 2:")
+# Problem 2:
+print("The difference in age between Laura and Max is", 15 - 6, "years")
+
+
+print(SECTION + "Problem 3:")
+# Problem 3:
+print("George is", 22 / 6, "times older than Laura")
+
+
+print(SECTION + "Problem 4:")
+# Problem 4:
+print("If I drive", 60, "miles per hour, and I drive for", 4, "hours, I will drive", 60 * 4, "miles")
