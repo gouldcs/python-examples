c825bd836a09252b52fe967693b46a308edc384e
Cameron Gould <56109943+gouldcs@users.noreply.github.com>
2021-01-15 17:51:26 -0800

    Create solutions1.py

---
+++ 1. Your First Code/solutions1.py
@@ -0,0 +1,87 @@
+"""Welcome to the code-along! In each section, I will
+include a Python file with everything we do in the
+section so you have easy access to it all.
+"""
+SECTION = "\n***********************************\n"
+
+
+# 1. Hello, World!
+print("1. Hello, World!", SECTION + "Example 1.1:")
+# Example 1:
+print("Hello, World!")
+
+
+# Example 2 does not work!
+# print(Hello, World!) 
+
+
+print(SECTION + "Example 1.3:")
+# Example 3:
+print("Hello,")
+print("World!")
+
+
+print(SECTION + "Example 1.4:")
+# Example 4:
+print("Hello,", end="")
+print("World!")
+
+
+print(SECTION + "Example 1.5:")
+# Example 5:
+print("Hello,", end="")
+print("World!")
+print("The sun is shining!")
+
+
+print(SECTION + "Example 1.6:")
+# Example 6:
+print("Hello,", end="")
+print("World!", end="")
+print("The sun is shining!", end="")
+
+
+print(SECTION + "Example 1.7:")
+# Example 7:
+print("Hello," ,  "World!" , "The sun is shining!")
+
+print(SECTION + "Example 1.8:")
+# Example 8:
+print("Hello," +  "World!" + "The sun is shining!")
+
+
+print(SECTION + "Example 1.9:")
+# Example 9:
+print("Hel" + "lo," ,  "Wor" + "ld!" , "The" , "sun" , "is" , "shining!")
+
+print(SECTION)
+print("Further Exploration Solutions")
+
+
+print(SECTION + "Problem 1:")
+# Problem 1:
+print("Hello, Universe!")
+
+
+print(SECTION + "Problem 2:")
+# Problem 2:
+print("John")
+print("Doe")
+print("50")
+
+
+print(SECTION + "Problem 3:")
+# Problem 3:
+print("John", end=" ")
+print("Doe", end=" ")
+print("50", end="")
+
+
+print(SECTION + "Problem 4:")
+# Problem 4:
+print("John", "Doe", "50")
+
+
+print(SECTION + "Problem 5:")
+# Problem 5:
+print("Man,", "I", "lo" + "ve", "Chick" + "-fil-" + "a!")
